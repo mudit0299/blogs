5591ae4a1fa40d48040093dcd21645d776af1602
Mudit Gupta <muditgupta.rocks@gmail.com>
2020-01-28 23:02:15 +0530

    moving to database

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const app = express();
+var _=require('lodash')
 
 app.set('view engine', 'ejs');
 
@@ -14,9 +15,12 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-
+let posts=[];
 app.get("/",(req,res)=>{
-  res.render("home",{homeContent: homeStartingContent})
+  
+  res.render("home",{homeContent: homeStartingContent,
+  posts:posts
+  })
 });
 app.get("/about",(req,res)=>{
   res.render("about",{aboutContent: aboutContent})
@@ -28,9 +32,38 @@ app.get("/compose",(req,res)=>{
   res.render("compose");
 });
 app.post("/compose",(req,res)=>{
-  var input=req.body.inputBlog
-  console.log(input);
+  const post={
+    title:req.body.posttitle,
+    content:req.body.postbody
+    
+  };
+  posts.push(post);
+  res.redirect('/');
+  
 });
+app.get('/posts/:blogid',(req,res)=>{
+  var q=_.lowerCase(req.params.blogid);
+  // var p=(posts.some(function(post){
+  //  return  post.title===q
+  // }))
+  // if(p===true){
+  //   console.log("match found")
+  // }else{
+  //   console.log("not found")
+  posts.forEach((post)=>{
+    
+    var blogtitle=_.lowerCase(post.title)
+    if(blogtitle==q){
+      res.render("post",{
+        title:post.title,
+        content:post.content
+      })
+    }else{
+      console.log("no match found")
+    }
+  })
+  
+})
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
